a3e7ab4b006b356bc044b1bfa578bd5b2424d107
Jared Hanson <jaredhanson@gmail.com>
2013-09-16 13:41:21 -0700

    Test cases for async parse and error handling.

--- test/test.parse.test.js
+++ test/test.parse.test.js
@@ -39,3 +39,91 @@ describe('test grant that calls parse', function() {
   });
   
 });
+
+describe('test grant that calls async parse', function() {
+  
+  var grant = {};
+  grant.request = function(req, done) {
+    return done(null, { foo: 'bar' });
+  }
+  grant.response = function(txn, res, next) {}
+  
+  describe('with a parse callback', function() {
+    var err, obj;
+  
+    before(function(done) {
+      var test = new Test(grant);
+      test.parse(function(e, o) {
+        err = e;
+        obj = o;
+        done();
+      }).authorize();
+    });
+  
+    it('should not error', function() {
+      expect(err).to.be.null;
+    });
+  
+    it('should parse', function() {
+      expect(obj.foo).to.be.equal('bar');
+    });
+  });
+  
+});
+
+describe('test grant that calls parse that throws an exception', function() {
+  
+  var grant = {};
+  grant.request = function(req, done) {
+    throw new Error('something was thrown');
+  }
+  grant.response = function(txn, res, next) {}
+  
+  describe('with a parse callback', function() {
+    var err, obj;
+  
+    before(function(done) {
+      var test = new Test(grant);
+      test.parse(function(e, o) {
+        err = e;
+        obj = o;
+        done();
+      }).authorize();
+    });
+  
+    it('should not error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('something was thrown');
+    });
+  });
+  
+});
+
+describe('test grant that calls async parse that encounters an error', function() {
+  
+  var grant = {};
+  grant.request = function(req, done) {
+    return done(new Error('something went wrong'));
+  }
+  grant.response = function(txn, res, next) {}
+  
+  describe('with a parse callback', function() {
+    var err, obj;
+  
+    before(function(done) {
+      var test = new Test(grant);
+      test.parse(function(e, o) {
+        err = e;
+        obj = o;
+        done();
+      }).authorize();
+    });
+  
+    it('should not error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('something went wrong');
+    });
+  });
+  
+});
+
