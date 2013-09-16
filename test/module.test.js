c8564998075abf95b520cd13828209db06a6d1f0
Jared Hanson <jaredhanson@gmail.com>
2013-09-16 12:15:27 -0700

    Add test cases for request and response.

---
+++ test/module.test.js
@@ -0,0 +1,9 @@
+var oauth2orizeGrant = require('..');
+
+describe('chai-oauth2orize-grant', function() {
+  
+  it('should export function', function() {
+    expect(oauth2orizeGrant).to.be.a('function');
+  });
+  
+});
