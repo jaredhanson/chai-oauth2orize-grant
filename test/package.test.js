ff249188074fead4cdfe5b01e15a018e9558f808
Jared Hanson <jaredhanson@gmail.com>
2013-11-26 18:07:42 -0800

    Reorganize tests.

---
+++ test/package.test.js
@@ -0,0 +1,9 @@
+var oauth2orize = require('..');
+
+describe('chai-oauth2orize-grant', function() {
+  
+  it('should export function', function() {
+    expect(oauth2orize).to.be.a('function');
+  });
+  
+});
