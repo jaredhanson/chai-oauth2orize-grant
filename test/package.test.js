629e34c9306d54a6bf9529b99b2df5b8893314db
Jared Hanson <jaredhanson@gmail.com>
2017-02-27 09:43:29 -0800

    Update tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,11 +1,30 @@
 /* global describe, it, expect */
 
-var oauth2orize = require('..');
+var helper = require('..')
+  , Test = require('../lib/test');
 
 describe('chai-oauth2orize-grant', function() {
   
   it('should export function', function() {
-    expect(oauth2orize).to.be.a('function');
+    expect(helper).to.be.a('function');
+  });
+  
+  describe('used as a chai helper', function() {
+    var chai = {};
+    helper(chai);
+  
+    it('should add oauth2orize helper to chai', function() {
+      expect(chai.oauth2orize).to.be.an('object');
+      expect(chai.oauth2orize.grant).to.be.a('function');
+    });
+  
+    describe('when invoked', function() {
+      var test = chai.oauth2orize.grant({});
+    
+      it('should return test wrapper', function() {
+        expect(test).to.be.an.instanceOf(Test);
+      });
+    });
   });
   
 });
