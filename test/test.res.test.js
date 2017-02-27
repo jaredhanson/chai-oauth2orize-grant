9a41ed0067409f5826ead504567efc9982bbb6ce
Jared Hanson <jaredhanson@gmail.com>
2017-02-27 10:11:47 -0800

    Add test coverage for error handling.

--- test/test.res.test.js
+++ test/test.res.test.js
@@ -2,7 +2,7 @@
 
 var Test = require('../lib/test');
 
-describe('toreparing a response', function() {
+describe('preparing a response', function() {
   
   var grant = {};
   grant.request = function(req) {};
@@ -23,6 +23,42 @@ describe('toreparing a response', function() {
           eres = r;
           done();
         }).decide();
+      });
+    
+      it('should get same response from preparation and end', function() {
+        expect(pres).to.be.equal(eres);
+      });
+    });
+    
+  });
+  
+});
+
+
+describe('preparing a response for error handling', function() {
+  
+  var grant = {};
+  grant.request = function(req) {};
+  grant.response = function(txn, res, next) {
+    res.end();
+  };
+  grant.error = function(err, txn, res, next) {
+    res.end('Error: ' + err.message);
+  };
+  
+  describe('sync', function() {
+    
+    describe('and dispatches', function() {
+      var pres, eres;
+    
+      before(function(done) {
+        var test = new Test(grant);
+        test.res(function(res) {
+          pres = res;
+        }).end(function(r) {
+          eres = r;
+          done();
+        }).error(new Error('Danger, Will Robinson!'));
       });
     
       it('should get same response from preparation and end', function() {
--- test/test.txn.test.js
+++ test/test.txn.test.js
@@ -58,3 +58,64 @@ describe('preparing a transaction', function() {
   });
   
 });
+
+
+describe('preparing a transaction for error handling', function() {
+  
+  var grant = {};
+  grant.request = function(req) {};
+  grant.response = function(txn, res, next) {
+    res.end(txn.ext.hello);
+  };
+  grant.error = function(err, txn, res, next) {
+    res.end('Error: ' + err.message);
+  };
+  
+  describe('sync', function() {
+    
+    describe('and dispatches', function() {
+      var res;
+    
+      before(function(done) {
+        var test = new Test(grant);
+        test.txn(function(txn) {
+          txn.ext = {};
+          txn.ext.hello = 'World';
+        }).end(function(r) {
+          res = r;
+          done();
+        }).error(new Error('Danger, Will Robinson!'));
+      });
+    
+      it('should send response', function() {
+        expect(res.body).to.be.equal('Error: Danger, Will Robinson!');
+      });
+    });
+    
+  });
+  
+  describe('async', function() {
+    
+    describe('and dispatches', function() {
+      var res;
+    
+      before(function(done) {
+        var test = new Test(grant);
+        test.txn(function(txn, done) {
+          txn.ext = {};
+          txn.ext.hello = 'World';
+          process.nextTick(done);
+        }).end(function(r) {
+          res = r;
+          done();
+        }).error(new Error('Danger, Will Robinson!'));
+      });
+    
+      it('should send response', function() {
+        expect(res.body).to.be.equal('Error: Danger, Will Robinson!');
+      });
+    });
+    
+  });
+  
+});
